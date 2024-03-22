384c85dfef3994e838a8f357ab3bf04d403625f2
ariel-marques <65095659+ariel-marques@users.noreply.github.com>
2024-03-22 20:28:37 -0300

    Mobile and desktop typed.js version

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -1,7 +1,12 @@
-const typed = new Typed('.txt-animado', {
-        
+const typedDesktop = new Typed('.txt-animado', {
     strings: ['ARIEL MARQUES'],
     typeSpeed: 80,
     backSpeed: 130,
     loop: false,
-  });
+});
+const typedMobile = new Typed('.txt-animado-mobile', {
+    strings: ['ARIEL MARQUES'],
+    typeSpeed: 80,
+    backSpeed: 130,
+    loop: false,
+});
