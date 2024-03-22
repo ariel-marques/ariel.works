3664d6c6cc1d17a8c85c7adcb701dc8f046207a9
ariel-marques <65095659+ariel-marques@users.noreply.github.com>
2024-03-22 20:06:41 -0300

    Mobile typed.js

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -5,3 +5,10 @@ const typed = new Typed('.txt-animado', {
     backSpeed: 130,
     loop: false,
   });
+const typed = new Typed('.txt-animado-mobile', {
+        
+    strings: ['ARIEL MARQUES'],
+    typeSpeed: 80,
+    backSpeed: 130,
+    loop: false,
+  });
