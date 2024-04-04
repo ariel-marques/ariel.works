3af84969f952eef26935456d4df76de045feebd6
ariel-marques <65095659+ariel-marques@users.noreply.github.com>
2024-04-04 15:40:43 -0300

    Change the showCursor

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -3,18 +3,32 @@ async function loadSVG(url) {
   const svgContent = await response.text();
   return svgContent;
 }
-
+// Função principal para iniciar o Typed.js
 async function iniciarTyped(elementSelector, svgUrl) {
   const strings = ['ARIEL MARQUES', await loadSVG(svgUrl)];
 
-  const typed = new Typed(elementSelector, {
+  // Definir a variável 'typed' fora do escopo da função para garantir acesso aos callbacks
+  let typed;
+
+  // Iniciar Typed.js com as strings carregadas
+  typed = new Typed(elementSelector, {
     strings: strings,
-    showCursor: false,
     typeSpeed: 80,
     backSpeed: 130,
     loop: true,
-    onComplete: function () {
-      document.getElementById('svg-container').innerHTML = strings[1];
+    showCursor: true, // Exibe o cursor de texto inicialmente
+    onComplete: function() {
+      // Ao completar a animação, oculta o cursor de texto
+      if (typed && typed.cursor) {
+        typed.cursor.style.display = 'none';
+        document.getElementById('svg-container').innerHTML = strings[1];
+      }
+    },
+    onBegin: function() {
+      // Ao iniciar a animação, exibe o cursor de texto
+      if (typed && typed.cursor) {
+        typed.cursor.style.display = 'inline-block';
+      }
     }
   });
 }
