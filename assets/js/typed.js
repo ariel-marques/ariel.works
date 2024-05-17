5508ab61f1e788453c803562e39e8973acf0c05c
ariel-marques <65095659+ariel-marques@users.noreply.github.com>
2024-05-17 18:45:12 -0300

    Back to old version before faded logo typed.js

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -3,37 +3,29 @@ async function loadSVG(url) {
   const svgContent = await response.text();
   return svgContent;
 }
-
+// Função principal para iniciar o Typed.js
 async function iniciarTyped(elementSelector, svgUrl) {
-  const svgContent = await loadSVG(svgUrl);
-  const element = document.querySelector(elementSelector);
+  const strings = ['ARIEL MARQUES', await loadSVG(svgUrl)];
 
-  const typed = new Typed(elementSelector, {
-    strings: ['ARIEL MARQUES'],
+  // Definir a variável 'typed' fora do escopo da função para garantir acesso aos callbacks
+  let typed;
+
+  // Iniciar Typed.js com as strings carregadas
+  typed = new Typed(elementSelector, {
+    strings: strings,
     typeSpeed: 80,
     backSpeed: 130,
-    backDelay: 1000,  // Pequeno atraso antes de iniciar o backspace
-    startDelay: 500,
-    loop: false,
-    showCursor: true,
+    loop: true,
+    showCursor: true, // Exibe o cursor de texto inicialmente
     onComplete: function() {
-      // Quando o texto estiver completamente digitado, iniciar o backspace
-      setTimeout(() => {
-        typed.stop();  // Parar Typed.js
-        simulateBackspace(element, 'ARIEL MARQUES', () => {
-          element.innerHTML = `<div class="svg-container">${svgContent}</div>`;
-          setTimeout(() => {
-            document.querySelector('.svg-container').classList.add('fade-in');
-            // Parar o cursor de piscar
-            if (typed.cursor) {
-              typed.cursor.style.display = 'none';
-            }
-          }, 50); // Pequeno atraso para garantir que o SVG foi inserido antes de aplicar o fade-in
-        });
-      }, 1000); // Iniciar o backspace após 1 segundo
+      // Ao completar a animação, oculta o cursor de texto
+      if (typed && typed.cursor) {
+        typed.cursor.style.display = 'none';
+        document.getElementById('svg-container').innerHTML = strings[1];
+      }
     },
-    preStringTyped: function(arrayPos) {
-      // Exibir o cursor antes de digitar a string
+    onBegin: function() {
+      // Ao iniciar a animação, exibe o cursor de texto
       if (typed && typed.cursor) {
         typed.cursor.style.display = 'inline-block';
       }
@@ -41,19 +33,6 @@ async function iniciarTyped(elementSelector, svgUrl) {
   });
 }
 
-function simulateBackspace(element, text, callback) {
-  let currentText = text;
-  const interval = setInterval(() => {
-    if (currentText.length > 0) {
-      currentText = currentText.slice(0, -1);
-      element.innerHTML = currentText;
-    } else {
-      clearInterval(interval);
-      callback();
-    }
-  }, 130); // Velocidade do backspace
-}
-
-// Iniciar Typed.js para os dois seletores
 iniciarTyped('.txt-animado', 'https://ariel.works/assets/img/logo.svg');
+
 iniciarTyped('.txt-animado-mobile', 'https://ariel.works/assets/img/logo.svg');
